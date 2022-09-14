3db0b4575eae5ecb12671b00587ad3ef50218115
MAX <maksimdemidovru@mail.ru>
2022-09-14 12:31:26 +0300

    add Ex52

--- Ex50/Program.cs
+++ Ex50/Program.cs
@@ -1,11 +1,19 @@
-﻿void SearchIndex(int[,] table)
-{
-    Console.WriteLine("Введите искомое число: ");
-    bool line = int.TryParse(Console.ReadLine(), out int a);
-    if (!line)
+﻿Console.WriteLine("Введите количество столбцов в массиве: ");
+bool line = int.TryParse(Console.ReadLine(), out int lineinmassive);
+Console.WriteLine("Введите количество строк в массиве: ");
+bool column = int.TryParse(Console.ReadLine(), out int columninmassive);
+
+Console.WriteLine("Введите искомое число: ");
+bool number = int.TryParse(Console.ReadLine(), out int a);
+    if (!line || !column || !number || columninmassive<1 || lineinmassive<1)
     {
         Console.WriteLine("Не верные введённые данные, попробуйте поменять значение.");
     }
+
+void SearchIndex(int[,] table)
+{
+    int notfound = 0;
+    int quanity = 0;
     for (int i = 0; i < table.GetLength(0); i++)
     {
         for (int j = 0; j < table.GetLength(1); j++)
@@ -14,8 +22,17 @@
             {
                 Console.WriteLine($"Число {a} в двумерном массиве находится под индексом ({i};{j})");
             }
+            else
+            {
+                notfound ++;
+            }
+            quanity = table.GetLength(1) * table.GetLength(0);
         }
     }
+    if (notfound == quanity)
+    {
+        Console.WriteLine("Число не найдено");
+    } 
 }
 
 int[,] NewDoubleMassive(int[,] table)
@@ -41,10 +58,7 @@ void PrintMassive(int[,] table)
         Console.WriteLine();
     }
 }
-int[,] result = new int[5, 5];
-int[,] res = NewDoubleMassive(result);
-SearchIndex(res);
-PrintMassive(res);
-
-
-
+int[,] doublemassive = new int[columninmassive, lineinmassive];
+int[,] resultmassive = NewDoubleMassive(doublemassive);
+PrintMassive(resultmassive);
+SearchIndex(resultmassive);
--- Ex52/Program.cs
+++ Ex52/Program.cs
@@ -8,29 +8,48 @@ if (!line || !column || columninmassive<1 || lineinmassive<1)
     Console.WriteLine("Не верные введённые данные, попробуйте поменять значение.");
 }
 
-double[,] resultmassive = new double[lineinmassive,columninmassive];
-PrintMassive(New2xMassive(resultmassive));
+int[,] resultmassive = new int[lineinmassive,columninmassive];
+int[,] printmassive = New2xMassive(resultmassive);
+PrintMassive(printmassive);
 
-double[,] New2xMassive(double[,]table)
+int[,] New2xMassive(int[,]table)
 {
     for (int i = 0; i < table.GetLength(0); i++)
     {
         for (int j = 0; j < table.GetLength(1); j++)
         {
-            table[i,j] = new Random().Next(-10,100)/10.0;
+            table[i,j] = new Random().Next(1,10);
         }
     }
     return table;
 }
 
-void PrintMassive(double[,]table)
+void PrintMassive(int[,]table)
 {
     for (int i = 0; i < table.GetLength(0); i++)
     {
         for (int j = 0; j < table.GetLength(1); j++)
         {
-            Console.Write($" |{table[i, j]}| ");
+            Console.Write($"  {table[i, j]}  ");
         }
         Console.WriteLine();
     }
 }
+
+void ArithmeticMean(int[,]table)
+{
+      Console.WriteLine("Среднее арифметическое каждого столбца равно: ");
+    double[] mean = new double[columninmassive];
+    for (int i = 0; i < table.GetLength(1); i++)
+    {
+        for (int j = 0; j < table.GetLength(0); j++)
+        {
+            if(i <= lineinmassive)
+            {
+                mean[i] += table[j,i];
+            }
+        }
+        Console.Write($"{(mean[i]/lineinmassive)}   ");
+    }
+}
+ArithmeticMean(resultmassive);
