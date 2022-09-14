ba31bbe2d7365511e9fa84dbb9d91c5aaeffbbcf
MAX <maksimdemidovru@mail.ru>
2022-09-14 09:34:17 +0300

    add Ex50

--- Ex47/Program.cs
+++ Ex47/Program.cs
@@ -9,14 +9,26 @@ if (!line || !column)
 }
 
 double[,] result = new double[a,b];
-New2xMassive(result);
-void New2xMassive(double[,]table)
+PrintMassive(New2xMassive(result));
+
+double[,] New2xMassive(double[,]table)
 {
     for (int i = 0; i < table.GetLength(0); i++)
     {
         for (int j = 0; j < table.GetLength(1); j++)
         {
             table[i,j] = new Random().Next(-10,100)/10.0;
+        }
+    }
+    return table;
+}
+
+void PrintMassive(double[,]table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
             Console.Write($"|{table[i, j]}|");
         }
         Console.WriteLine();
---
+++ Ex50/Program.cs
@@ -0,0 +1,50 @@
+﻿void SearchIndex(int[,] table)
+{
+    Console.WriteLine("Введите искомое число: ");
+    bool line = int.TryParse(Console.ReadLine(), out int a);
+    if (!line)
+    {
+        Console.WriteLine("Не верные введённые данные, попробуйте поменять значение.");
+    }
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            if (table[i, j] == a)
+            {
+                Console.WriteLine($"Число {a} в двумерном массиве находится под индексом ({i};{j})");
+            }
+        }
+    }
+}
+
+int[,] NewDoubleMassive(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return table;
+}
+
+void PrintMassive(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($"|{table[i, j]}|");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] result = new int[5, 5];
+int[,] res = NewDoubleMassive(result);
+SearchIndex(res);
+PrintMassive(res);
+
+
+
